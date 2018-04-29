210eaaf23e58761485ac719353095c00bde06df1
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-29 01:36:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ function lowerCaseDrivers(drivers) {
 //   return x;
 // }
 
-// function attributesToPhrase(drivers) {
-//
-//   x = drivers.map(function (driver) {
-//     name = driver[:name];
-//     hometown = driver[:hometown];
-//     return "y";
-//     // return "${name} is from ${hometown}";
-//   });
-//   return x;
-// }
+function attributesToPhrase(drivers) {
+
+  x = drivers.map(function (driver) {
+    return 2;
+    // name = driver[:name];
+    // hometown = driver[:hometown];
+    // return "y";
+    // return "${name} is from ${hometown}";
+  });
+  return x;
+}
