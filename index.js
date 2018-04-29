8c9f47c866830e10561c98cb01d53379127a796e
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-29 01:07:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+
+  newEngineers.map(function (driver) {
+    return driver.toLowerCase());
+  });
+
+
+  return Object.assign({}, lc, { equipment: 'Laptop' });
+}
+
+const equippedEngineers = newEngineers.map(function (eng) {
+  return Object.assign({}, eng, { equipment: 'Laptop' });
+});
