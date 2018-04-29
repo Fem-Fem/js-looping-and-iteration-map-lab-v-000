ca5f50ee40caa9b7d93fcdd1857ca3ac08c56911
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-29 01:08:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function lowerCaseDrivers(drivers) {
 
   drivers.map(function (driver) {
     return driver.toLowerCase());
-  });
+  });}
 
-
-  return Object.assign({}, lc, { equipment: 'Laptop' });
-}
-
-const equippedEngineers = newEngineers.map(function (eng) {
-  return Object.assign({}, eng, { equipment: 'Laptop' });
-});
+//
+//   return Object.assign({}, lc, { equipment: 'Laptop' });
+// }
+//
+// const equippedEngineers = newEngineers.map(function (eng) {
+//   return Object.assign({}, eng, { equipment: 'Laptop' });
+// });
